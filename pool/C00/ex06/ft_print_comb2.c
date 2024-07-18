cb902da99e1b3d8396e043275670744da80f2cbb
zerith <terry.bourillon@outlook.fr>
2024-07-18 09:18:15 +0200

    correction

--- pool/C00/ex06/ft_print_comb2.c
+++ pool/C00/ex06/ft_print_comb2.c
@@ -6,7 +6,7 @@
 /*   By: tbourill <terry.bourillon@outlook.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 18:59:51 by tbourill          #+#    #+#             */
-/*   Updated: 2024/07/18 08:08:56 by tbourill         ###   ########.fr       */
+/*   Updated: 2024/07/18 08:32:19 by tbourill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,51 +14,49 @@
 
 void print(int a,int b,int c,int d)
 {
-	write(1, &a, 1);
-	write(1, &b, 1);
-	write(1, &" ", 1);
-	write(1, &c, 1);
 	write(1, &d, 1);
+	write(1, &c, 1);
+	write(1, &" ", 1);
+	write(1, &b, 1);
+	write(1, &a, 1);
 	if (d!=58)
 	{
 		write(1, &", ", 1);
 	}
 }
 
-void try(int a,int b,int c,int d)
+void try(int *a,int *b,int *c,int *d)
 {
-	if (a == 10)
+	if (*a == 10)
 	{
-		a=0;
-		b++;
+		*a=0;
+		*b++;
 	}
-	if (b == 10)
+	if (*b == 10)
 	{
-		b = 0;
-		c++;
+		*b = 0;
+		*c++;
 	}
-	if (c == 10)
+	if (*c == 10)
 	{
-		c = 0;
-		d++;
+		*c = 0;
+		*d++;
 	}
 }
 void	ft_print_comb2(void)
 {
-	int a;
-	int b;
-	int c;
-	int d;
+	int	tmp;
 
-	a = 0;
-	b = 0;
-	c = 0;
-	d = 0;
-	while (d!=10)
+	tmp=0;
+	int *a=&tmp;
+	int *b=&tmp;
+	int *c=&tmp;
+	int *d=&tmp;
+	while (*d!=10)
 	{
-		try(a,b,c,d);
-		print(a + 48, b + 48,c + 48, d + 48);
-		a++;
+		try(&a,&b,&c,&d);
+		print(*a + 48, *b + 48,*c + 48, *d + 48);
+		*a++;
 	}
 }
 
