ca55bea612bbd057d26d04191f38ccfc86d352e7
zerith <terry.bourillon@outlook.fr>
2024-07-19 05:49:18 +0200

    ex07 C00

--- pool/C00/ex07/ft_putnbr.c
+++ pool/C00/ex07/ft_putnbr.c
@@ -6,18 +6,33 @@
 /*   By: tbourill <terry.bourillon@outlook.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 16:50:46 by tbourill          #+#    #+#             */
-/*   Updated: 2024/07/18 17:06:48 by tbourill         ###   ########.fr       */
+/*   Updated: 2024/07/19 05:47:24 by tbourill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void len(char*d)
+#include <unistd.h>
+
+void	print_nb(int b)
 {
-    int i;
-    i = 0;
-    while (d[i]!="\n")
-    {
-        i++;
-    }
-    return(i);
+	write(1, &b, 1);
 }
 
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		write(1, "-",1 );
+		nb = -nb;
+	}
+	if (nb > 10)
+	{
+		ft_putnbr(nb / 10);
+	}
+	print_nb(nb % 10 + 48);
+}
+
+int	main(void)
+{
+	ft_putnbr(-492);
+	return (0);
+}
