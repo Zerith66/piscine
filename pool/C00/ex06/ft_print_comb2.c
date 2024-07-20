27f3448626e3d9804af2ebd953e42306a3ab5e3d
Terry Bourillon <tbourill@c2r7p1.42perpignan.fr>
2024-07-20 11:08:22 +0200

    corr

--- pool/C00/ex06/ft_print_comb2.c
+++ pool/C00/ex06/ft_print_comb2.c
@@ -22,24 +22,25 @@ void	ft_print_comb2(void)
 	int	a;
 	int	b;
 
-	a = 0;
+	a = 1;
 	b = 0;
 	while (b <= 98)
 	{
-		if (a <= 99)
+		print(b / 10 + 48);
+		print(b % 10 + 48);
+		write(1, " ", 1);
+		print(a / 10 + 48);
+		print(a % 10 + 48);
+		if (b != 98)
 		{
-			print(b / 10 + 48);
-			print(b % 10 + 48);
-			write(1, " ", 1);
-			print(a / 10 + 48);
-			print(a % 10 + 48);
-			if (b != 98)
-			{
-				write(1, ", ", 2);
-			}
+			write(1, ", ", 2);
 		}
 		a++;
-		b++;
+		if(a == 100 )
+		{
+			a = b + 2;
+			b++;
+		}
 	}
 }
 
