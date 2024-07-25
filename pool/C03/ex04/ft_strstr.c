ee2e7f395be4b1296468494999628bc7f60c2b22
zerith <terry.bourillon@outlook.fr>
2024-07-25 09:44:09 +0200

    correctec and add C04

--- pool/C03/ex04/ft_strstr.c
+++ pool/C03/ex04/ft_strstr.c
@@ -6,7 +6,23 @@
 /*   By: tbourill <terry.bourillon@outlook.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 21:59:32 by tbourill          #+#    #+#             */
-/*   Updated: 2024/07/24 22:19:06 by tbourill         ###   ########.fr       */
+/*   Updated: 2024/07/25 09:41:49 by tbourill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+
+	i = -1;
+	while (str[i] && to_find[i])
+	{
+		i++;
+		if (!(str[i] == to_find[i]))
+		{
+			return (i);
+			i++;
+		}
+	}
+	return ('\0');
+}
