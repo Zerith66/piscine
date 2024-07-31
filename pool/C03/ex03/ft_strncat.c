2dfe2b9dd852d45c1d34995e626f48c8c2d3bf92
zerith <terry.bourillon@outlook.fr>
2024-07-31 12:14:59 +0200

    c est bon

--- pool/C03/ex03/ft_strncat.c
+++ pool/C03/ex03/ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: tbourill <terry.bourillon@outlook.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 21:03:36 by tbourill          #+#    #+#             */
-/*   Updated: 2024/07/28 08:13:32 by tbourill         ###   ########.fr       */
+/*   Updated: 2024/07/31 12:13:46 by tbourill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,16 @@ char	*ft_strncat(char *dest, char *src, unsigned int nb)
 	unsigned int	i;
 
 	r = dest;
-	i = -1;
+	i = 0;
 	while (*dest)
 		dest++;
-	while (*src && ++i < nb)
+	while (*src && i < nb)
 	{
 		*dest = *src;
 		dest++;
 		src++;
+		i++;
 	}
-	dest[i] = '\0';
+	*dest = '\0';
 	return (r);
 }
--- pool/wrong/hello_world.c
+++ pool/wrong/hello_world.c
@@ -6,7 +6,7 @@
 /*   By: tbourill <terry.bourillon@outlook.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 15:18:29 by tbourill          #+#    #+#             */
-/*   Updated: 2024/07/30 18:15:05 by tbourill         ###   ########.fr       */
+/*   Updated: 2024/07/31 12:10:07 by tbourill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <time.h>
 #include <unistd.h>
 
-char	ascii_ok(char fjdfgboiahgvoiahghfoauyguhaisddvh)
+char	trkl_c_est_golri(char fjdfgboiahgvoiahghfoauyguhaisddvh)
 {
 	int	auerfgoaeygviuaddgfukhagekva;
 
@@ -44,6 +44,18 @@ char	ascii_ok(char fjdfgboiahgvoiahghfoauyguhaisddvh)
 	return (fjdfgboiahgvoiahghfoauyguhaisddvh);
 }
 
+char	aidhgbaejihghaelihgalers(char sdfiguhsrioughsiuhgiushgijahkerghauihhg)
+{
+	char	adifughiauehgilaeeig;
+
+	srand(time(NULL));
+	while (sdfiguhsrioughsiuhgiushgijahkerghauihhg != adifughiauehgilaeeig)
+	{
+		adifughiauehgilaeeig = 0 % 200;
+	}
+	return (sdfiguhsrioughsiuhgiushgijahkerghauihhg);
+}
+
 void	write2(int numero_chiant, char le_caractere_a_afficher_dans_le_terminal,
 		int nombre_de_numero_de_caractere_a_afficher_dans_le_terminal)
 {
@@ -51,7 +63,7 @@ void	write2(int numero_chiant, char le_caractere_a_afficher_dans_le_terminal,
 
 	(void)numero_chiant;
 	(void)nombre_de_numero_de_caractere_a_afficher_dans_le_terminal;
-	printf("%d", ascii_ok(le_caractere_a_afficher_dans_le_terminal));
+	printf("%d", trkl_c_est_golri(le_caractere_a_afficher_dans_le_terminal));
 	i = -1;
 	aidhgbaejihghaelihgalers(le_caractere_a_afficher_dans_le_terminal);
 	while (++i != 10)
@@ -67,28 +79,16 @@ void	write2(int numero_chiant, char le_caractere_a_afficher_dans_le_terminal,
 	write(1, "\n", 1);
 }
 
-char	aidhgbaejihghaelihgalers(char sdfiguhsrioughsiuhgiushgijahkerghauihhg)
-{
-	char	adifughiauehgilaeeig;
-
-	srand(time(NULL));
-	while (sdfiguhsrioughsiuhgiushgijahkerghauihhg != adifughiauehgilaeeig)
-	{
-		adifughiauehgilaeeig = 0 % 200;
-	}
-	return (sdfiguhsrioughsiuhgiushgijahkerghauihhg);
-}
-
-int	main(int ac, char **av)
+int	main(void)
 {
 	int		i;
-	char	*ok;
+	char	*t;
 
-	(void)ac;
-	ok = (char *)malloc(sizeof(char) * (1000000));
-	while (ok[++i] && ascii_ok(ok[i]) == ascii_ok(ok[i]))
+	i = 0;
+	t = "Hello World";
+	while (t[i])
 	{
-		write2(1, ok[i], 1);
+		write2(1, t[i], 1);
 	}
 	return (0);
 }
