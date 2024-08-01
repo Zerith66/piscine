619a581fe9f2b998baaae84ce5fa67ce2051ae33
zerith <terry.bourillon@outlook.fr>
2024-08-01 14:18:06 +0200

    ok

--- pool/C05/ex03/ft_recursive_power.c
+++ pool/C05/ex03/ft_recursive_power.c
@@ -6,7 +6,7 @@
 /*   By: tbourill <terry.bourillon@outlook.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/27 16:43:37 by tbourill          #+#    #+#             */
-/*   Updated: 2024/07/29 11:47:55 by tbourill         ###   ########.fr       */
+/*   Updated: 2024/08/01 13:31:45 by tbourill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ int	ft_recursive_power(int nb, int power)
 {
 	if (power < 0)
 		return (0);
-	if (power == 0 && nb > 0)
+	if (power == 0 && nb >= 0)
 		return (1);
 	return (nb * ft_recursive_power(nb, power - 1));
 }
